62f3795470707e64810b9160af82a08e4bd9fef6
Tesfay-Teshome <tesfix2017@gmail.com>
2024-03-30 12:01:06 +0300

    base

--- Dj_CRUD/EC/eco/views.py
+++ Dj_CRUD/EC/eco/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
 def home(request):
-    return render (request, "index.html")
+    return render (request, "eco/home.html")
